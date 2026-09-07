285063d1e7ac834f6dce2054b02461c0d01e618a
Victor Hugo <111150704+VictorHugoSimon@users.noreply.github.com>
2026-09-07 20:16:22 -0300

    feat(api): register harvest and CTT routes

--- apps/api/src/worker.ts
+++ apps/api/src/worker.ts
@@ -1,8 +1,10 @@
 import app from './index';
 import { registerEconomicsRoutes } from './economics';
 import { registerOperationsRoutes } from './operations';
+import { registerHarvestRoutes } from './harvest';
 
 registerEconomicsRoutes(app as never);
 registerOperationsRoutes(app as never);
+registerHarvestRoutes(app as never);
 
 export default app;
