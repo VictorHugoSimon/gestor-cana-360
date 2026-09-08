a3cc2814d8d41b6bd8abdb0477a12c9e4e64da88
Victor Hugo <111150704+VictorHugoSimon@users.noreply.github.com>
2026-09-08 08:14:05 -0300

    feat(api): register climate and satellite routes

--- apps/api/src/worker.ts
+++ apps/api/src/worker.ts
@@ -4,11 +4,13 @@ import { registerOperationsRoutes } from './operations';
 import { registerHarvestRoutes } from './harvest';
 import { registerAssetsRoutes } from './assets';
 import { registerAgronomyRoutes } from './agronomy';
+import { registerClimateSatelliteRoutes } from './climateSatellite';
 
 registerEconomicsRoutes(app as never);
 registerOperationsRoutes(app as never);
 registerHarvestRoutes(app as never);
 registerAssetsRoutes(app as never);
 registerAgronomyRoutes(app as never);
+registerClimateSatelliteRoutes(app as never);
 
 export default app;
